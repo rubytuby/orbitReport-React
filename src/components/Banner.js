321749d7cb2409fb162b0cba42ce20aed804e91b
Liz <adammvehige@gmail.com>
2023-07-19 17:29:50 -0500

    finished work on banner and buttons, updated app.js

---
+++ src/components/Banner.js
@@ -0,0 +1,21 @@
+const Banner = () => {
+    return (
+        <div>
+            <header>Orbit Report</header>
+            <p>Use the buttons to see the satellites in the orbit type.</p>
+        </div>
+    );
+};
+
+export default Banner;
+
+
+
+
+
+
+
+
+
+
+
